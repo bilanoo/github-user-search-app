f5471cbc37980a15f1000f2428671f3af295d24e
bilanoo <blalkhan94@gmail.com>
2023-06-28 17:49:07 +0100

    Dev information implemented

--- src/DevInformation/index.tsx
+++ src/DevInformation/index.tsx
@@ -1,11 +1,45 @@
-import { Box } from "@mui/material";
-import { DevInformationContainer } from "./style";
+import { Box, Typography } from "@mui/material";
+import {
+  InformationValue,
+  BioContainer,
+  CreationDate,
+  DevInformationContainer,
+  DevProfileContainer,
+  ProfileName,
+  ProfileNameandDateContainer,
+  InformationContainer,
+  InformationTitle,
+  ReposAndFollowersContainer,
+  UsernameContainer,
+} from "./style";
 import ProfilePicture from "./ProfilePicture";
 
 const DevInformation = () => {
   return (
     <DevInformationContainer item xs={12} id="dev-information-container">
       <ProfilePicture />
+      <DevProfileContainer>
+        <ProfileNameandDateContainer>
+          <ProfileName>The Octocat</ProfileName>
+          <CreationDate>Joined 25 Jan 2011</CreationDate>
+        </ProfileNameandDateContainer>
+        <UsernameContainer>@octocat</UsernameContainer>
+        <BioContainer>This profile has no bio</BioContainer>
+        <ReposAndFollowersContainer>
+          <InformationContainer>
+            <InformationTitle>Repos</InformationTitle>
+            <InformationValue>8</InformationValue>
+          </InformationContainer>
+          <InformationContainer>
+            <InformationTitle>Followers</InformationTitle>
+            <InformationValue>9644</InformationValue>
+          </InformationContainer>
+          <InformationContainer>
+            <InformationTitle>Following</InformationTitle>
+            <InformationValue>9</InformationValue>
+          </InformationContainer>
+        </ReposAndFollowersContainer>
+      </DevProfileContainer>
     </DevInformationContainer>
   );
 };
--- src/DevInformation/style.tsx
+++ src/DevInformation/style.tsx
@@ -1,4 +1,4 @@
-import { Box, Grid, styled } from "@mui/material";
+import { Box, Grid, Typography, styled } from "@mui/material";
 
 export const DevInformationContainer = styled(Grid)(() => ({
   "&": {
@@ -8,5 +8,108 @@ export const DevInformationContainer = styled(Grid)(() => ({
     minWidth: "100%",
     minHeight: "50vh",
     marginTop: "20px",
+  },
+}));
+
+export const DevProfileContainer = styled(Box)(() => ({
+  "&": {
+    marginLeft: "auto",
+    minWidth: "60vh",
+  },
+}));
+
+export const ProfileNameandDateContainer = styled(Box)(() => ({
+  "&": {
+    marginTop: "40px",
+    minWidth: "100%",
+    display: "flex",
+    justifyContent: "space-between",
+  },
+}));
+
+export const ProfileName = styled(Typography)(() => ({
+  "&": {
+    fontSize: "26px",
+    fontWeight: "bold",
+    color: "white",
+    fontFamily: "Space Mono, monospace",
+  },
+}));
+
+export const CreationDate = styled(Typography)(() => ({
+  "&": {
+    fontSize: "18px",
+    fontWeight: "bold",
+    color: "white",
+    fontFamily: "Space Mono, monospace",
+    marginRight: "40px",
+    marginTop: "auto",
+  },
+}));
+
+export const UsernameContainer = styled(Box)(() => ({
+  "&": {
+    marginTop: "10px",
+    minWidth: "100%",
+    display: "flex",
+    justifyContent: "space-between",
+    color: "#0079ff",
+  },
+}));
+
+export const BioContainer = styled(Box)(() => ({
+  "&": {
+    marginTop: "30px",
+    minWidth: "100%",
+    color: "#C7CAD1",
+    fontWeight: "bold",
+  },
+}));
+
+export const ReposAndFollowersContainer = styled(Box)(() => ({
+  "&": {
+    display: "flex",
+    flexDirection: "row",
+    borderRadius: "10px",
+    backgroundColor: "#141D2F",
+    minWidth: "90%",
+    minHeight: "20%",
+    fontWeight: "bold",
+    marginTop: "30px",
+    marginRight: "40px",
+  },
+}));
+
+export const InformationContainer = styled(Box)(() => ({
+  "&": {
+    display: "flex",
+    flexDirection: "column",
+    borderRadius: "10px",
+    backgroundColor: "#141D2F",
+    minHeight: "20%",
+    fontWeight: "bold",
+    marginTop: "20px",
+    marginRight: "40px",
+  },
+}));
+
+export const InformationTitle = styled(Typography)(() => ({
+  "&": {
+    fontSize: "13px",
+    color: "white",
+    fontFamily: "Space Mono, monospace",
+    marginRight: "40px",
+    marginLeft: "37px",
+  },
+}));
+
+export const InformationValue = styled(Typography)(() => ({
+  "&": {
+    fontSize: "28px",
+    fontWeight: "bold",
+    color: "white",
+    fontFamily: "Space Mono, monospace",
+    marginRight: "40px",
+    marginLeft: "37px",
   },
 }));
